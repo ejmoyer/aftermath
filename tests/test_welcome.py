a2fd8ac23b590b8fd6a691ce323964e621449fb3
Eric Moyer <ejmoyer3@comcast.net>
2020-03-25 13:16:01 -0400

    Set up some future project structure and tests that should've been there in the previous branch.

---
+++ tests/test_welcome.py
@@ -0,0 +1,10 @@
+import aftermath
+from aftermath import create_app
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+def test_welcome(client):
+    response = client.get('/')
+    assert response
