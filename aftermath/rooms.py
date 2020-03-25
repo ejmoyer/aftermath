f52603f7a3780d768e5796ef92e1604b111136c3
Eric Moyer <ejmoyer3@comcast.net>
2020-03-25 16:23:47 -0400

    Fixed issues with routing, now directs properly.

--- aftermath/rooms.py
+++ aftermath/rooms.py
@@ -6,15 +6,15 @@ bp = Blueprint('rooms', __name__)
 
 @bp.route('/game/<room_name>')
 def room(room_name):
+    descriptions = {
+        'portal': 'I am the portal room.', #maybe make these objects with more info?
+        'overgrown': 'I am the overgrown room.',
+        'temple': 'I am the temple room.',
+        'gate': 'I am the gate room.',
+        'underworld': 'I am the underworld room.'
+        }
+
     return render_template('/game/room.html',
                 title=room_name,
-                room_description="test"
-)
-
-    routes = {
-        '/portal': room('portal'),
-        '/overgrown': room('overgrown'),
-        '/temple': room('temple'),
-        '/gate': room('gate'),
-        '/underworld': room('underworld')
-        }
+                room_description=descriptions[room_name]
+                )
