92a86cba850dca74335eaa2a39af769a82a78591
Eric Moyer <ejmoyer3@comcast.net>
2020-03-26 16:21:15 -0400

    Changed welcome to use proper save game attribute name, started on tests for interactiveness, started on creating proper if/else block for handling post requests.

--- aftermath/rooms.py
+++ aftermath/rooms.py
@@ -1,17 +1,19 @@
 from flask import (
-    Blueprint, render_template, request, g, session
+    Blueprint, render_template, request, g, session, redirect, url_for
 )
 
 bp = Blueprint('rooms', __name__)
 
 class Room():
-    room_commands = 'observe, save, change room, help'.upper()
-
     def __init__(self, name, desc, unique_command):
         self.name = name
         self.desc = desc
         self.unique_command = unique_command.upper()
 
+        if self.name == 'portal':
+            self.room_commands = 'observe, save, change room, help'.upper()
+        else:
+            self.room_commands = 'observe, save, go back, help'.upper()
 
 @bp.route('/game/<room_name>', methods=('GET', 'POST'))
 def room(room_name):
@@ -22,7 +24,17 @@ def room(room_name):
         'gate': Room('gate', 'I am the gate room.', 'approach gate, '),
         'underworld': Room('underworld', 'I am the underworld room.', 'approach light, ')
         }
-    
+
+    if request.method == 'POST':
+        action = request.form['action']
+
+        if action == "save":
+            session['save_game'] = room_name
+        elif action == "go back":
+            return redirect(url_for(".room", room_name='portal'))
+        else:
+            pass
+
     return render_template('/game/room.html',
                 title=room_name,
                 room_description=descriptions[room_name].desc,
