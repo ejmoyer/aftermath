2a6afd02c89b2be55d817a065887bd9909e4fddf
Eric Moyer <ejmoyer3@comcast.net>
2020-03-27 14:30:44 -0400

    Added and completed help dialogue, started on replacing placeholder descriptions with real descriptions.

--- aftermath/rooms.py
+++ aftermath/rooms.py
@@ -5,38 +5,49 @@ from flask import (
 bp = Blueprint('rooms', __name__)
 
 class Room():
-    def __init__(self, name, desc, unique_command):
+    def __init__(self, name, desc, unique_command, unique_answer):
         self.name = name
         self.desc = desc
         self.unique_command = unique_command.upper()
-
-        if self.name == 'portal':
-            self.room_commands = 'observe, save, change room, help'.upper()
-        else:
-            self.room_commands = 'observe, save, go back, help'.upper()
+        self.unique_answer = unique_answer
+        self.room_commands = 'save, go to (room name), help'.upper()
 
 @bp.route('/game/<room_name>', methods=('GET', 'POST'))
 def room(room_name):
     descriptions = {
-        'portal': Room('portal', 'I am portal', 'examine statue, '),
-        'overgrown': Room('overgrown', 'I am the overgrown room.', 'examine tree, '),
-        'temple': Room('temple', 'I am the temple room.', 'listen closer, '),
-        'gate': Room('gate', 'I am the gate room.', 'approach gate, '),
-        'underworld': Room('underworld', 'I am the underworld room.', 'approach light, ')
+        'portal': Room('portal', """A large connecting room, with many doors on all sides leading to
+        other unknown realms. In the center lies an ancient statue, it's head
+        and right arm missing. It's left arm is broken off and laying to the
+        side, with a small broken scale in hand.""", 'examine statue, ', 'test'),
+        'overgrown': Room('overgrown', """
+        The area has a huge tree in the center, with a large
+        hole bored into the center of it, looks like some sort of
+        energy in the middle of the hole. The rest of the area is
+        overgrown and covered in roots from the large tree.""", 'examine tree, ', 'test'),
+        'temple': Room('temple', 'I am the temple room.', 'listen closer, ', 'test'),
+        'gate': Room('gate', 'I am the gate room.', 'approach gate, ', 'test'),
+        'underworld': Room('underworld', 'I am the underworld room.', 'approach light, ', 'test')
         }
 
     if request.method == 'POST':
-        action = request.form['action']
+        action = request.form['action'].lower()
 
         if action == "save":
             session['save_game'] = room_name
-        elif action == "go back":
-            return redirect(url_for(".room", room_name='portal'))
+        elif "go to" in action:
+            place = action.split( ).pop()
+            return redirect(url_for(".room", room_name=place))
+        elif action == "help":
+            g.help = 1
+            print(descriptions[room_name].unique_command)
+        elif action in descriptions[room_name].unique_command.lower():
+            g.unique_command = 1
         else:
-            pass
+            print('no')
 
     return render_template('/game/room.html',
                 title=room_name,
                 room_description=descriptions[room_name].desc,
-                room_commands=descriptions[room_name].unique_command + descriptions[room_name].room_commands
+                room_commands=descriptions[room_name].unique_command + descriptions[room_name].room_commands,
+                unique_answer=descriptions[room_name].unique_answer
                 )
